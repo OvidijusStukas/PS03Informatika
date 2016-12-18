2da2b5bafddc3b85ef07ae306b7995b91dde9716
Vytautas <vytautas.boznis@gmail.com>
2016-12-18 23:12:51 +0200

    Added user level passthow to JS, added extra security for buttons

--- src/main/java/edu/informatika/semestrinis/controller/MessagingController.java
+++ src/main/java/edu/informatika/semestrinis/controller/MessagingController.java
@@ -1,8 +1,20 @@
 package edu.informatika.semestrinis.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("messages")
 public class MessagingController {
+
+    @RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
+    public ModelAndView index() {
+
+        ModelAndView modelAndView = new ModelAndView("messaging/index");
+
+        return modelAndView;
+    }
 
 }
